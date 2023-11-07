445d443f2b746d4818e606f6a0216902d63bc720
habbkad <habibkadiri52@gmail.com>
2023-11-07 21:28:10 +0000

    Updated picupload

--- controllers/nft_controller.js
+++ controllers/nft_controller.js
@@ -5,7 +5,6 @@ const Nft = require("../models/nft_model");
 //access   public
 
 exports.getNfts = async (req, res, next) => {
-  console.log("connect");
   try {
     const allNfts = await Nft.find();
 
@@ -28,34 +27,39 @@ exports.getNft = async (req, res, next) => {};
 //access   private
 
 exports.createNft = async (req, res, next) => {
-  console.log(req.file);
-  // const {
-  //   title,
-  //   details,
-  //   creator,
-  //   ether_link,
-  //   ipfs_link,
-  //   meta_data,
-  //   current_bid,
-  //   auctionTime,
-  //   nft_picture,
-  //   hotbid,
-  //   sold,
-  //   tags,
-  // } = req.body;
-  // try {
-  //   const isPresent = await Nft.findOne({ title });
-  //   if (isPresent) {
-  //     return res.status(201).json({ error: `nft with same title present` });
-  //   }
+  console.log(req.body);
+  const {
+    title,
+    details,
+    creator,
+    ether_link,
+    ipfs_link,
+    meta_data,
+    current_bid,
+    auctionTime,
+    nft_picture,
+    hotbid,
+    sold,
+    tags,
+  } = req.body;
+  try {
+    const isPresent = await Nft.findOne({ title });
+    if (isPresent) {
+      return res.status(201).json({ error: `nft with same title present` });
+    }
 
-  //   const newNft = new Nft(req.body);
-  //   await newNft.save();
+    const newNft = new Nft(req.body);
+    await newNft.save();
 
-  //   res.status(200).send({ message: `nft uploaded successfully` });
-  // } catch (error) {
-  //   res.status(404).send({ message: `error occured`, error: error.message });
-  // }
+    let images = [];
+    for (let i of req.files) {
+      images = [...images, `http://localhost:8000/${i.filename}`];
+    }
+    await Nft.findByIdAndUpdate(newNft._id, { nft_picture: images });
+    res.status(200).send({ message: `nft uploaded successfully` });
+  } catch (error) {
+    res.status(404).send({ message: `error occured`, error: error.message });
+  }
 };
 
 //desc     delet nfts
--- routes/nft_routes.js
+++ routes/nft_routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const {
   getNft,
   getNfts,
@@ -7,11 +8,22 @@ const {
   searchNft,
   updateNft,
 } = require("../controllers/nft_controller");
-// const multer = require("multer");
-// const upload = multer({ dest: "../uploads/" });
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: "./uploads/",
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+// Set up storage for uploaded files
+
+// Create the multer instance
+
 const Router = express.Router();
 
-Router.route("/").get(getNfts).post(createNft);
+Router.route("/").get(getNfts).post(upload.array("files", 3), createNft);
 
 Router.route("/:id").put(updateNft).delete(deleteNft).get(getNft);
 
